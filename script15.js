1cfe9359f700033d9b1520c1b1996a71a648eb11
Ivo Gulin <69385144+IvoGulin@users.noreply.github.com>
2021-10-27 14:03:42 +0200

    Add files via upload

--- script15.js
+++ script15.js
@@ -16,3 +16,36 @@ function addition()
 	
 	document.getElementById("demo72").innerHTML = z.toFixed(2);
 }
+function suma()
+{
+	const person = [];
+	person[0] = "John";
+	person[1] = "Doe";
+	person[2] = 46; 
+	document.getElementById("demo73").innerHTML =
+	person[0] + " " + person.length;
+}
+
+
+const x = document.getElementById("demo74");
+
+function getLocation() 
+{
+  if (navigator.geolocation) 
+  {
+	navigator.geolocation.getCurrentPosition(showPosition);
+  } 
+  else 
+  { 
+	x.innerHTML = "Geolocation is not supported by this browser.";
+  }
+}
+
+function showPosition(position) 
+{
+  x.innerHTML = "Latitude: " + position.coords.latitude + 
+  "<br>Longitude: " + position.coords.longitude;
+}
+function myFunction() {
+  alert("I am an alert box!");
+}
